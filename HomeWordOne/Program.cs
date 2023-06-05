d85e0eba591312a1083340952ee916bb8a83c191
#GorshokPeep <nikitagorshkov58@gmail.com>
2023-06-05 19:48:49 +0300

    Добавьте файлы проекта.

---
+++ HomeWordOne/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HomeWordOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int value = rnd.Next(0, 100);
+            int valueKeyBoard = 0;
+
+            do
+            {
+                valueKeyBoard = int.Parse(Console.ReadLine());
+                if (value > valueKeyBoard)
+                {
+                    Console.WriteLine("Число больше чем вы написали");
+                } else if (value < valueKeyBoard)
+                {
+                    Console.WriteLine("Число меньше чем вы написали");
+                } else
+                {
+                    Console.WriteLine("Вы угадали число");
+                }
+            } while (value != valueKeyBoard);
+
+            Console.WriteLine(value);
+        }
+    }
+}
